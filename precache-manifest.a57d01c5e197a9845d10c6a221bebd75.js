2e81a8dc05c66cf4b564f0216de6638419eccf8a
José Vitor <jszvitor@gmail.com>
2020-03-18 15:12:27 -0300

    feat(master): Environment installation

---
+++ precache-manifest.a57d01c5e197a9845d10c6a221bebd75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699c28b4a3551ec0a1031141137a0dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec574866bb63b7a4730a",
+    "url": "/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "88b9cb2f0b734edc91d1",
+    "url": "/static/js/2.3a60236e.chunk.js"
+  },
+  {
+    "revision": "ec574866bb63b7a4730a",
+    "url": "/static/js/main.0a01cf2a.chunk.js"
+  },
+  {
+    "revision": "7ef6924db03ec9753c79",
+    "url": "/static/js/runtime~main.2efed672.js"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/Banner@jszvitor.baf6b40a.jpg"
+  },
+  {
+    "revision": "f352b8bf8bc32f3e9af2158e174742b8",
+    "url": "/static/media/bg3.f352b8bf.jpg"
+  },
+  {
+    "revision": "936ffdd006ace99fcecd7ccf1eb16125",
+    "url": "/static/media/christian.936ffdd0.png"
+  }
+]);
